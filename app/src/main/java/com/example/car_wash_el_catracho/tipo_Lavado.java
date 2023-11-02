4daf72166dc7266399b404a90920794df65ef220
OldFarmComputer <yeissonosorio6@gamil.com>
2023-11-02 14:12:02 -0600

    v2

--- app/src/main/java/com/example/car_wash_el_catracho/tipo_Lavado.java
+++ app/src/main/java/com/example/car_wash_el_catracho/tipo_Lavado.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
@@ -13,7 +14,7 @@ import android.widget.TextView;
 public class tipo_Lavado extends AppCompatActivity {
     Button btnvol;
 
-    Spinner lugar;
+    Spinner lugar,hora;
 
     TextView tipo,titul;
     @Override
@@ -24,19 +25,52 @@ public class tipo_Lavado extends AppCompatActivity {
         tipo=(TextView) findViewById(R.id.tvprecio);
         titul = (TextView) findViewById(R.id.tituloL);
         lugar = (Spinner) findViewById(R.id.tipoL);
+        hora = (Spinner) findViewById(R.id.HoraL);
 
-        ArrayAdapter<CharSequence> adp = ArrayAdapter.createFromResource(this,R.array.combo_opción, android.R.layout.simple_dropdown_item_1line);
+        ArrayAdapter<CharSequence> adp = ArrayAdapter.createFromResource(this,R.array.combo_opción, android.R.layout.simple_expandable_list_item_1);
         lugar.setAdapter(adp);
+
+        ArrayAdapter<CharSequence> adpH = ArrayAdapter.createFromResource(this,R.array.Hora, android.R.layout.simple_expandable_list_item_1);
+        hora.setAdapter(adpH);
 
         String servicio = getIntent().getStringExtra("tipo");
 
+
         if (servicio.equals("Completo")){
             titul.setText("Lavado Completo");
-            tipo.setText("Precio: L.150");
         } else if (servicio.equals("fuera")) {
-            tipo.setText("Precio: L.100");
             titul.setText("Lavado por fuera");
         }
+
+        lugar.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (servicio.equals("fuera")){
+                    if(lugar.getSelectedItemId()==1) {
+                        tipo.setText("Precio: L.100");
+                    } else if (lugar.getSelectedItemId()==2) {
+                        tipo.setText("Precio: L.150");
+                    }
+                    else{
+                        tipo.setText("");
+                    }
+                } else if (servicio.equals("Completo")) {
+                    if(lugar.getSelectedItemId()==1) {
+                        tipo.setText("Precio: L.150");
+                    } else if (lugar.getSelectedItemId()==2) {
+                        tipo.setText("Precio: L.200");
+                    }
+                    else {
+                        tipo.setText("");
+                    }
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         btnvol =(Button) findViewById(R.id.btnAtras);
 
